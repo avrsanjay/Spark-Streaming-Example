16ff6d02133ad960117dcb25ff14afa537687908
avrsanjay <avrsanjay@gmail.com>
2016-02-25 15:23:21 -0800

    Working version for custom data generator

--- spark/.worksheet/src/testplay.scala
+++ spark/.worksheet/src/testplay.scala
@@ -1,11 +1,18 @@
 import scala.math.BigInt
 import java.nio.ByteBuffer
 import org.apache.hadoop.hbase.util.Bytes
+import scala.util.matching.Regex
 
 
-object testplay {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(143); 
-	val s = "$114.85".toString();System.out.println("""s  : String = """ + $show(s ));$skip(55); 
-	val r = s.replace('$', ' ').replace('\"', ' ').trim();System.out.println("""r  : String = """ + $show(r ));$skip(20); 
-	val q = r.toDouble;System.out.println("""q  : Double = """ + $show(q ))}
-	
+object testplay {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(187); 
+		val topics = "MARKET1,MARKET2,MARKET3";System.out.println("""topics  : String = """ + $show(topics ));$skip(62); 
+		val result = scala.collection.mutable.MutableList[String]();System.out.println("""result  : scala.collection.mutable.MutableList[String] = """ + $show(result ));$skip(60); 
+		val topicIterator = topics.split(",").toIterable.iterator;System.out.println("""topicIterator  : Iterator[String] = """ + $show(topicIterator ));$skip(145); 
+		while(topicIterator.hasNext){
+		  val topic = topicIterator.next()
+		  println("topic:"+topic)
+		  val market = topic
+		  result += market
+		};$skip(20); 
+    println(result)}
 }
--- spark/src/main/scala/com/cloudwick/streaming/spark/TestConsumer1.scala
+++ spark/src/main/scala/com/cloudwick/streaming/spark/TestConsumer1.scala
@@ -1,3 +1,4 @@
+//code which can take streaming from Kafka and update values in HBase by read/write
 package com.cloudwick.streaming.spark
 
 import org.apache.spark._
@@ -22,92 +23,120 @@ import org.apache.spark.SparkConf
 import scala.collection.mutable
 import java.nio.ByteBuffer
 
-
-
-
 object TestConsumer1{
 	def main(args: Array[String]) {
-		if (args.length < 4) {
-			System.err.println("Usage: KafkaWordCount <zkQuorum> <group> <topics> <numThreads>")
+		if (args.length < 5) {
+			System.err.println("Usage: KafkaWordCount <zkQuorum> <group> <topics> <table_name> <numThreads>")
 			System.exit(1)
 		}
-		val Array(zkQuorum, group, topics, numThreads) = args
+		val Array(zkQuorum, group, topics, table_name, numThreads) = args
 		val ssc = new StreamingContext("local[2]", "Test", Seconds(5))
 		ssc.checkpoint("/user/spark/checkpoint")
 		val sparkConf = new SparkConf().setAppName("Hbase Sample")
 		val sc = ssc.sparkContext
 
-		
+
 		val topicMap = topics.split(",").map((_, numThreads.toInt)).toMap
 		val Json = KafkaUtils.createStream(ssc,zkQuorum,group,topicMap)
 		.window(Seconds(5), Seconds(5))
 		.map(_._2)
 
-		
-		Json.foreachRDD(rdd => {
-			rdd.foreach { x =>
-  			val data = parse(x,false)
-			  val StockSymbol = compact(data \\ "StockSymbol")
-			  val StockSymbolF = StockSymbol.replace('"', ' ').trim()
-			  val StockNumber = compact(data \\ "StockNumber")       
-			  val Mp = compact(data \\ "Mp")  
-			  val MpF = Mp.replace('$', ' ').replace('\"', ' ').trim()
-			  val Bp = compact(data \\ "Bp")
-			  val BpF = Bp.replace('$', ' ').replace('\"', ' ').trim()
-			  val Ap = compact(data \\ "Ap")
-			  val ApF = Ap.replace('$', ' ').replace('\"', ' ').trim()
-			  val BQ = compact(data \\ "BQ")         
-			  val Aq = compact(data \\ "Aq")         
-			  val Vol = compact(data \\ "Vol")
-			  val rowId = compact(data \\ "rowId")
-			  
-			  val conf = new HBaseConfiguration()
-		    val admin = new HBaseAdmin(conf)
-			  val table = new HTable(conf, "mytable1")
-			  	
-  			val s = new Scan().setReversed(true)
-  			val filter = new PrefixFilter(Bytes.toBytes(StockNumber+"_"))
-  			s.setFilter(filter)
-  			s.addFamily(Bytes.toBytes("mycf"))
-  			val rs = table.getScanner(s)
-  			val rit = rs.iterator()
-  			if(rit.hasNext()){
-  			  val nxt = rit.next()
-  			  val oldMp = Bytes.toString(nxt.getValue(Bytes.toBytes("mycf"), Bytes.toBytes("column2")))
-  			  val oldBp = Bytes.toString(nxt.getValue(Bytes.toBytes("mycf"), Bytes.toBytes("column3")))
-  			  val oldAp = Bytes.toString(nxt.getValue(Bytes.toBytes("mycf"), Bytes.toBytes("column4")))
-  			  val NewMp = (MpF.toDouble + oldMp.replace('\"', ' ').trim().toDouble).toString()
-  			  val NewBp = (BpF.toDouble + oldBp.replace('\"', ' ').trim().toDouble).toString()
-  			  val NewAp = (ApF.toDouble + oldAp.replace('\"', ' ').trim().toDouble).toString()
-  			  val ts = System.currentTimeMillis()
-  			  
-  			  val theput = new Put(Bytes.toBytes(StockNumber.toString()+"_"+ts.toString()),ts)
-			    theput.add(Bytes.toBytes("mycf"),Bytes.toBytes("column1"),Bytes.toBytes(StockSymbol))
-			    theput.add(Bytes.toBytes("mycf"),Bytes.toBytes("column2"),Bytes.toBytes(NewMp))
-  			  theput.add(Bytes.toBytes("mycf"),Bytes.toBytes("column3"),Bytes.toBytes(NewBp))
-			    theput.add(Bytes.toBytes("mycf"),Bytes.toBytes("column4"),Bytes.toBytes(NewAp))
-			    theput.add(Bytes.toBytes("mycf"),Bytes.toBytes("column5"),Bytes.toBytes(BQ))
-			    theput.add(Bytes.toBytes("mycf"),Bytes.toBytes("column6"),Bytes.toBytes(Aq))
-			    theput.add(Bytes.toBytes("mycf"),Bytes.toBytes("column7"),Bytes.toBytes(Vol))
-			    theput.add(Bytes.toBytes("mycf"),Bytes.toBytes("column8"),Bytes.toBytes(rowId))
-			    table.put(theput)
-  			}
-  			else {					
-			    val ts = System.currentTimeMillis()
-  			  val theput = new Put(Bytes.toBytes(StockNumber.toString()+"_"+ts.toString()),ts)
-			    theput.add(Bytes.toBytes("mycf"),Bytes.toBytes("column1"),Bytes.toBytes(StockSymbol))
-			    theput.add(Bytes.toBytes("mycf"),Bytes.toBytes("column2"),Bytes.toBytes(MpF))
-  			  theput.add(Bytes.toBytes("mycf"),Bytes.toBytes("column3"),Bytes.toBytes(BpF))
-			    theput.add(Bytes.toBytes("mycf"),Bytes.toBytes("column4"),Bytes.toBytes(ApF))
-			    theput.add(Bytes.toBytes("mycf"),Bytes.toBytes("column5"),Bytes.toBytes(BQ))
-			    theput.add(Bytes.toBytes("mycf"),Bytes.toBytes("column6"),Bytes.toBytes(Aq))
-			    theput.add(Bytes.toBytes("mycf"),Bytes.toBytes("column7"),Bytes.toBytes(Vol))
-			    theput.add(Bytes.toBytes("mycf"),Bytes.toBytes("column8"),Bytes.toBytes(rowId))
-			    table.put(theput)
-  			}
- 			}
-		})
+		startCompute(Json,table_name)
 		ssc.start()
-    ssc.awaitTermination()
+		ssc.awaitTermination()
+
+		/******************************The following code is the implementations for functions*****************************/
+		//computation starts here
+		def startCompute(stream:org.apache.spark.streaming.dstream.DStream[String],table_name:String) = {
+			Json.foreachRDD(rdd => {
+				rdd.foreach { x =>
+			  	//the data is in JSON so pass it to function_parse function to get extracted data
+				  val (stockSymbol,stockNumber,mP,bP,aP,bQ,aQ,vol,rowid) = function_parse(x)
+  				//create HBase connection
+				  val (conf,admin,table) = connect2hbase(table_name)
+  				//get a list of rows for the row key "stockNumber" and an iterator to go through them
+				  val iterator = getRowsByKey(stockNumber,table)			
+				  /*
+					 * since the list will be in descending order, we will just use the first row and extract 
+					 * required values to be used to update incoming data
+				 	*/
+				  if(iterator.hasNext()){
+  					val (oldMp,oldBp,oldAp) = getOldValuesByColumn(iterator.next())
+  					//calculate new values to update the running total based on the old values
+					  val (updated_mp,updated_bp,updated_ap) = calculateNewValues(mP,bP,aP,oldMp,oldBp,oldAp)
+  					//insert the data
+	  				insert_data(stockSymbol,stockNumber,updated_mp,updated_bp,updated_ap,bQ,aQ,vol,rowid,table)
+				  }
+				  else {
+					  //insert the data
+					  insert_data(stockSymbol,stockNumber,mP,bP,aP,bQ,aQ,vol,rowid,table)
+				  }
+			  }
+			})
+		}
+
+		def function_parse(x:String) = {
+			val data = parse(x,false)
+			  val StockSymbol = compact(data \\ "StockSymbol")
+				val StockSymbolF = StockSymbol.replace('"', ' ').trim()
+				val StockNumber = compact(data \\ "StockNumber")   
+				val StockNumberF = StockNumber.replace('"', ' ').trim()
+				val Mp = compact(data \\ "Mp")  
+				val MpF = Mp.replace('$', ' ').replace('\"', ' ').trim()
+				val Bp = compact(data \\ "Bp")
+				val BpF = Bp.replace('$', ' ').replace('\"', ' ').trim()
+				val Ap = compact(data \\ "Ap")
+				val ApF = Ap.replace('$', ' ').replace('\"', ' ').trim()
+				val BQ = compact(data \\ "BQ")
+				val BQF = BQ.replace('\"', ' ').trim()
+				val Aq = compact(data \\ "Aq")
+				val AqF = Aq.replace('\"', ' ').trim()
+				val Vol = compact(data \\ "Vol")
+				val VolF = Vol.replace('\"', ' ').trim()
+				val rowId = compact(data \\ "rowId")
+				val rowIdF = rowId.replace('\"', ' ').trim()
+				//sending back the formatted content of the JSON string
+				(StockSymbolF,StockNumberF,MpF,BpF,ApF,BQF,AqF,VolF,rowIdF)
+		}
+		def connect2hbase(table_name:String) = {
+			val conf = new HBaseConfiguration()
+			val admin = new HBaseAdmin(conf)
+			val table = new HTable(conf, table_name)
+			(conf,admin,table)
+		}
+		def getRowsByKey(rowkey:String,table:HTable) = {
+			val s = new Scan().setReversed(true)
+			val filter = new PrefixFilter(Bytes.toBytes(rowkey+"_"))
+			s.setFilter(filter)
+			s.addFamily(Bytes.toBytes("mycf"))
+			val rs = table.getScanner(s)
+			(rs.iterator())
+		}
+		def getOldValuesByColumn(nxt:org.apache.hadoop.hbase.client.Result) = {
+			val oldMp = Bytes.toString(nxt.getValue(Bytes.toBytes("mycf"), Bytes.toBytes("col_Mp")))
+			val oldBp = Bytes.toString(nxt.getValue(Bytes.toBytes("mycf"), Bytes.toBytes("col_Bp")))
+			val oldAp = Bytes.toString(nxt.getValue(Bytes.toBytes("mycf"), Bytes.toBytes("col_Ap")))
+			(oldMp,oldBp,oldAp)
+		}
+
+		def calculateNewValues(c:String,d:String,e:String,oldMp:String,oldBp:String,oldAp:String) = {
+			val NewMp = (c.toDouble + oldMp.replace('\"', ' ').trim().toDouble).toString()
+			val NewBp = (d.toDouble + oldBp.replace('\"', ' ').trim().toDouble).toString()
+			val NewAp = (e.toDouble + oldAp.replace('\"', ' ').trim().toDouble).toString()
+			(NewMp,NewBp,NewAp)	  
+		}
+		def insert_data(StockSymbol:String,StockNumber:String,Mp:String,Bp:String,Ap:String,BQ:String,Aq:String,Vol:String,rowId:String,table:HTable) = {
+			val ts = System.currentTimeMillis()  			  
+			val theput = new Put(Bytes.toBytes(StockNumber+"_"+ts.toString()),ts)
+			theput.add(Bytes.toBytes("mycf"),Bytes.toBytes("col_SS"),Bytes.toBytes(StockSymbol))
+			theput.add(Bytes.toBytes("mycf"),Bytes.toBytes("col_Mp"),Bytes.toBytes(Mp))
+			theput.add(Bytes.toBytes("mycf"),Bytes.toBytes("col_Bp"),Bytes.toBytes(Bp))
+			theput.add(Bytes.toBytes("mycf"),Bytes.toBytes("col_Ap"),Bytes.toBytes(Ap))
+			theput.add(Bytes.toBytes("mycf"),Bytes.toBytes("col_BQ"),Bytes.toBytes(BQ))
+			theput.add(Bytes.toBytes("mycf"),Bytes.toBytes("col_Aq"),Bytes.toBytes(Aq))
+			theput.add(Bytes.toBytes("mycf"),Bytes.toBytes("col_Vol"),Bytes.toBytes(Vol))
+			theput.add(Bytes.toBytes("mycf"),Bytes.toBytes("col_rowId"),Bytes.toBytes(rowId))
+			table.put(theput)
+		}
 	}
 }
